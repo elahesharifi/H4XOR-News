a63378ccc8bab6ed59079225cc4243082185d614
elahe <el.sh192014@gmail.com>
2022-06-25 23:51:45 +0200

    add fetch func

--- H4XOR News/ContentView.swift
+++ H4XOR News/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
     var body: some View {
         NavigationView {
-            List(posts) { post in
+            List(networkManager.posts) { post in
                 Text(post.title)
             }
             .navigationBarTitle("H4XOR NEWS")
+        }
+        .onAppear{
+            self.networkManager.fetchData()
         }
     }
 }
@@ -24,13 +29,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Hello World")
-
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Hello World")
+//
+//]
